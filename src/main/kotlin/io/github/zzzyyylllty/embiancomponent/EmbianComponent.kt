276165052317c6c7b3880a996c848b4bf659b18c
zzzyyylllty <3631901756@qq.com>
2025-11-19 21:17:51 +0800

    修复版本号

--- src/main/kotlin/io/github/zzzyyylllty/embiancomponent/EmbianComponent.kt
+++ src/main/kotlin/io/github/zzzyyylllty/embiancomponent/EmbianComponent.kt
@@ -2,13 +2,24 @@ package io.github.zzzyyylllty.embiancomponent
 
 import io.github.zzzyyylllty.embiancomponent.tools.ComponentSetter
 import org.bukkit.Bukkit
+import taboolib.common.util.unsafeLazy
+import kotlin.getValue
 
 //import taboolib.module.nms.MinecraftVersion.versionId
 
 object EmbianComponent {
 
+
+    /**
+     * 当前运行的版本（数字版本），例如：1.8.8
+     */
+    val runningVersion by unsafeLazy {
+        val version = Bukkit.getServer().version.split("MC:")[1]
+        version.substring(0, version.length - 1).trim()
+    }
+
     val versionId: Int by lazy {
-        val split = Bukkit.getVersion().split(".")
+        val split = runningVersion.split(".")
         split[0].toInt()*10000 + split[1].toInt()*100 + split[2].toInt()
     }
 
